92538aeed001a6e39ab03023350082bf0f7bc1cb
techfresher <118924292+techfresher@users.noreply.github.com>
2024-07-31 16:59:58 +0100

    second commit

--- pages/models.py
+++ pages/models.py
@@ -11,7 +11,6 @@ class Team(models.Model):
     google_plus_link = models.URLField(max_length=150)
     created_date = models.DateTimeField(auto_now_add=True)
 
-
     
     def __str__(self):
         return self.first_name
