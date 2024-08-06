21ebdb8aad314529e1282a40548114295d133827
techfresher <118924292+techfresher@users.noreply.github.com>
2024-08-06 19:19:44 +0100

    staticfiles modifies

--- carzone/settings.py
+++ carzone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pdewjmq64!jdo(hgr(^844@d(=+2wee%i(k^k7#sufr8!jpoot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -142,17 +142,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]  
+    os.path.join(BASE_DIR, 'carzone/static')
+]
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
+ 
 # Default primary key field type
 # https://docs.djangoproject. com/en/4.2/ref/settings/#default-auto-field
 
